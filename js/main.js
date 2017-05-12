eb4a3054ad91648e058b476dfdde2b5053d13fea
fearlessfe <505380967@qq.com>
2017-05-12 17:40:46 +0800

    cat-click with two cats

--- js/main.js
+++ js/main.js
@@ -1,10 +1,44 @@
 window.onload=function(){
-	var nodeImg=document.getElementById('cat');
-	var nodeP=document.getElementById('times');
-	var count=0;
-	nodeImg.addEventListener('click',function(){
-		count++;
-		nodeP.innerHTML=count;
+	var cats=[
+		{
+			catName : 'cat1',
+			catSrc : 'images/1.jpg',
+			catCount : 0,
+		},
+		{
+			catName : 'cat2',
+			catSrc : 'images/2.jpg',
+			catCount : 0,
+		}
+	]
 
-	});
+	var ul=document.getElementById('cat-list');
+	
+	cats.forEach(function(elem,index,arr){
+		
+		var li=document.createElement('li');
+		var name=document.createElement('p');
+		var img=document.createElement('img');
+		var count=document.createElement('p');
+		
+
+		name.textContent=elem.catName;
+		img.src=elem.catSrc;
+		count.textContent=0;
+
+		img.addEventListener('click',function(){
+			elem.catCount++;
+			
+			count.innerHTML=elem.catCount;
+			
+		});
+		li.appendChild(name);
+		li.appendChild(img);
+		li.appendChild(count);
+		ul.appendChild(li);
+
+	})
+
+
+
 }
